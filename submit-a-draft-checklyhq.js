cd797119d2e3065948e12d6f447cd3295d5f2110
David Bass <bassd2@wwu.edu>
2022-10-06 15:42:44 -0700

    added headless:true

--- submit-a-draft-checklyhq.js
+++ submit-a-draft-checklyhq.js
@@ -15,7 +15,7 @@ const puppeteer = require('puppeteer');
     height: 768,
   };
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   await page.setViewport(screenSize);
 
