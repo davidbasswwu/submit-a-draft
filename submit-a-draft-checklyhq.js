8da7b964ff6ccd4173460b22012dcc7bdafaca6d
David Bass <bassd2@wwu.edu>
2022-10-06 13:57:23 -0700

    various changes/fixes, smaller viewPort

--- submit-a-draft-checklyhq.js
+++ submit-a-draft-checklyhq.js
@@ -12,56 +12,52 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch(
     {
       headless: false,
-      slowMo: 25, // slow down by _ ms 
+      slowMo: 55, // slow down by _ ms 
       devtools: true
     });
 
-  const page = await browser.newPage()
+  const page = await browser.newPage();
   const theDate = new Date();
   const date = theDate.toLocaleTimeString([], { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true });
-  const dateArray = date.split(', ')
-  const YYYYMMDD = dateArray[0]
-  const hhmm = dateArray[1]
-  console.log(YYYYMMDD)
-  console.log(hhmm)
+  const dateArray = date.split(', ');
+  const YYYYMMDD = dateArray[0];
+  const hhmm = dateArray[1];
+  console.log(YYYYMMDD);
+  console.log(hhmm);
   const draftTitle = 'Daily autotest ' + date;
 
-  const navigationPromise = page.waitForNavigation()
+  const navigationPromise = page.waitForNavigation();
 
-  await page.goto('https://library.wwu.edu/submit-draft-studio')
+  await page.goto('https://library.wwu.edu/submit-draft-studio');
 
-  await page.setViewport({ width: 1184, height: 1159 })
+  await page.setViewport({ width: 800, height: 600 });
 
-  await navigationPromise
+  await navigationPromise;
 
   console.log('process.env.FILE_PATH', process.env.FILE_PATH);
   console.log('x');
 
-  await page.waitForSelector('#edit-title-rws-draft')
-  await page.click('#edit-title-rws-draft')
-  await page.type('#edit-title-rws-draft', draftTitle)
-  console.log('x1');
+  await page.waitForSelector('#edit-title-rws-draft');
+  // await page.click('#edit-title-rws-draft')
+  await page.type('#edit-title-rws-draft', draftTitle);
 
-  // await page.type('#edit-details-other-information-rws-draft', '')
+  await page.waitForSelector('#edit-other-info-rws-draft');
+  await page.type('#edit-preferred-name-rws-draft', 'David Bass');
+  // await page.type('#edit-preferred-name-rws-draft', ' created by https://app.checklyhq.com/checks/1862047a-0f53-44fa-80bf-f536ece5b7e0/browser/edit by David Bass');
 
-  await page.type('#edit-preferred-name-rws-draft', 'David Bass')
+  await page.type('#edit-wwu-email-address-rws-draft-mail-1', 'bassd2@wwu.edu');
+  await page.type('#edit-wwu-email-address-rws-draft-mail-2', 'bassd2@wwu.edu');
 
-  await page.type('#edit-wwu-email-address-rws-draft-mail-1', 'bassd2@wwu.edu')
-  await page.type('#edit-wwu-email-address-rws-draft-mail-2', 'bassd2@wwu.edu')
-  console.log('x2');
-
-  await page.waitForSelector('#edit-due-date-rws-draft-date')
+  await page.waitForSelector('#edit-due-date-rws-draft-date');
   // await page.click('#edit-due-date-rws-draft-date')
-  await page.type('#edit-due-date-rws-draft-date', YYYYMMDD)
+  await page.type('#edit-due-date-rws-draft-date', YYYYMMDD);
 
-  await page.waitForSelector('#edit-due-date-rws-draft-time')
-  await page.type('#edit-due-date-rws-draft-time', '14:35 PM')
+  await page.waitForSelector('#edit-due-date-rws-draft-time');
+  await page.type('#edit-due-date-rws-draft-time', '14:35 PM');
 
-  console.log('x3');
-
-  await page.waitForSelector('#edit-academic-level-rws-draft')
-  await page.click('#edit-academic-level-rws-draft')
-  await page.select('#edit-academic-level-rws-draft', 'Undergraduate Student')
+  await page.waitForSelector('#edit-academic-level-rws-draft');
+  await page.click('#edit-academic-level-rws-draft');
+  await page.select('#edit-academic-level-rws-draft', 'Undergraduate Student');
 
   console.log('x4');
 
@@ -71,30 +67,30 @@ const puppeteer = require('puppeteer');
 
   console.log('x5');
 
-  await page.waitForSelector('#edit-draft-type-rws-draft-select')
-  await page.select('#edit-draft-type-rws-draft-select', 'Presentation')
+  await page.waitForSelector('#edit-draft-type-rws-draft-select');
+  await page.select('#edit-draft-type-rws-draft-select', 'Class Paper');
 
   console.log('x6');
 
-  await page.waitForSelector('#edit-academic-level-rws-draft')
-  await page.click('#edit-academic-level-rws-draft')
-  console.log('x7');
+  // await page.waitForSelector('#edit-academic-level-rws-draft')
+  // await page.click('#edit-academic-level-rws-draft')
+  // console.log('x7');
 
-  await page.waitForSelector('#edit-citation-style-rws-draft-select')
-  await page.select('#edit-citation-style-rws-draft-select', 'APA')
+  await page.waitForSelector('#edit-citation-style-rws-draft-select');
+  await page.select('#edit-citation-style-rws-draft-select', 'APA');
 
   console.log('a');
 
-  await page.waitForSelector('#edit-course-information-rws-draft')
-  await page.type('#edit-course-information-rws-draft', 'TST 101')
+  await page.waitForSelector('#edit-course-information-rws-draft');
+  await page.type('#edit-course-information-rws-draft', 'TST 101');
 
   console.log('b');
-  await page.waitForSelector('#edit-instructor-rws-draft')
-  await page.type('#edit-instructor-rws-draft', 'Gabe Gossett')
+  await page.waitForSelector('#edit-instructor-rws-draft');
+  await page.type('#edit-instructor-rws-draft', 'Gabe Gossett');
 
   console.log('c');
-  await page.waitForSelector('#edit-assignment-description-rws-draft')
-  await page.type('#edit-assignment-description-rws-draft', 'My assignment description does here.')
+  await page.waitForSelector('#edit-assignment-description-rws-draft');
+  await page.type('#edit-assignment-description-rws-draft', 'My assignment description does here.');
 
   console.log('e');
   // get the selector input type=file (for upload file)
@@ -119,34 +115,30 @@ const puppeteer = require('puppeteer');
 
   console.log('file uploaded');
 
-  await page.waitForSelector('#edit-other-info-rws-draft');
-  // await page.click('#edit-other-info-rws-draft')
-  await page.type('#edit-preferred-name-rws-draft', ' created by https://app.checklyhq.com/checks/1862047a-0f53-44fa-80bf-f536ece5b7e0/browser/edit ');
-
   console.log('g');
 
   await page.waitForSelector('#edit-how-did-you-learn-about-rws-draft-select');
   await page.select('#edit-how-did-you-learn-about-rws-draft-select', 'Instructor')
 
+  await page.waitForSelector('#edit-may-we-contact-rws-draft');
+  await page.evaluate(() => document.getElementById('edit-may-we-contact-rws-draft').click());
 
-  await page.waitForSelector('#edit-actions');
+  // await page.waitForSelector('#edit-actions');
   console.log('h');
 
   await page.waitForSelector('#edit-submit');
   console.log('i');
-  await page.screenshot({ path: 'before_submit.png', fullPage: true })
 
-  waitAndClick('#edit-submit', page);
+  // waitAndClick('#edit-submit', page);
 
   // await page.evaluate(()=>document.querySelector('#edit-submit').click());
-  // await page.evaluate(() => document.getElementById('edit-submit').click());
-
-  await page.screenshot({ path: 'after_submit.png', fullPage: true })
+  await page.evaluate(() => document.getElementById('edit-submit').click());
+  // await page.screenshot({ path: 'after_submit.png', fullPage: true })
 
   console.log('j');
 
-  await navigationPromise;
-  console.log('k');
+  // await navigationPromise;
+  // console.log('k');
 
   await browser.close()
 })()
